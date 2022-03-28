8b25c8e555566a2ee629145056d2359e18ce7c41
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-03-28 15:38:28 -0300

    03. Usando Spring Data
    Spring Boot API REST: construa uma API

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,30 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
 	
 	@RequestMapping("/topicos")	
-	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		Topico topico1 = new Topico("Dúvida", "Dúvida com Java8", new Curso("Java8", "Back-End"));
-		
-		return TopicoDto.converter(Arrays.asList(topico,topico,topico,topico1));
+	public List<TopicoDto> lista(String nomeCurso){
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+
 	}
 }
