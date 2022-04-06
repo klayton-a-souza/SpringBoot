cf33269f09d50f331f9fed78be4aa092920ad6d3
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-04-06 10:24:58 -0300

    03. Proteção com Spring Security
    Spring Boot API REST: Segurança da API, Cache e Monitoramento
    
    04. Liberando acesso aos endpoints públicos

--- src/main/java/br/com/alura/forum/cong/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/cong/security/SecurityConfigurations.java
@@ -1,11 +1,35 @@
 package br.com.alura.forum.cong.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	// Configurações de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	}
+
+	// Configurações de Autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+
+	}
+
+	// Configurações de recursos estaticos (js, css, imagens, etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+	}
 
 }
